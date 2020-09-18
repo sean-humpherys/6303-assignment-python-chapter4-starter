0a90b9f67bcbf36647c6e6b88e3e8d315976ca04
Sean Humpherys <shumpherys@wtamu.edu>
2020-09-18 11:19:11 -0500

    Instruction to students not to edit test file

--- testpayroll.py
+++ testpayroll.py
@@ -1,7 +1,9 @@
+# DO NOT EDIT THIS TEST FILE
 # This app tests the calc_payroll_tax() function
 # How to import a function from another file
 # You may see an error on line 6 until you finish creating calc_payroll_tax() in payroll.py. That's ok.
 # Notice calc_payroll_tax() is in payroll.py file, not the current app.py file
+
 from payroll import calc_payroll_tax
 
 calc_payroll_tax(100)
