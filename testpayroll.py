77d5055700e553d46be7801a9fcc54fedb17b926
Sean Humpherys <shumpherys@wtamu.edu>
2021-03-02 19:04:40 -0600

    updated comments

--- testpayroll.py
+++ testpayroll.py
@@ -2,7 +2,8 @@
 # This app tests the calc_payroll_tax() function
 # How to import a function from another file
 # You may see an error on line 6 until you finish creating calc_payroll_tax() in payroll.py. That's ok.
-# Notice calc_payroll_tax() is in payroll.py file, not the current app.py file
+# Notice calc_payroll_tax() is in payroll.py file, not the current file
+# The correct answers output by testpayroll.py are found in the instructions.
 
 from payroll import calc_payroll_tax
 
